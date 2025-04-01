19e402a2d56edcb88c53ff9b20aad7860df7d805
Davies Okpeta <davies19okpeta@gmail.com>
2025-04-01 18:20:24 +0100

    build fix

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,12 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-export function getUser() {
-  if (process.client) {
-    const token = window.localStorage.getItem("token");
-    if (!token) {
-      return { token: "" };
-    }
-    return { token };
-  } else return { token: "" };
-}
