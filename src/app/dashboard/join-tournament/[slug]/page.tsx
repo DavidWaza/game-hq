e836271b1278e8a6fc3a968fbe06aa98c87de556
Davies Okpeta <davies19okpeta@gmail.com>
2025-04-12 16:11:52 +0100

    mods

--- src/app/dashboard/join-tournament/[slug]/page.tsx
+++ src/app/dashboard/join-tournament/[slug]/page.tsx
@@ -2,14 +2,48 @@
 import CreateWagerBanner from "@/app/components/dashboard/join-wager-banner";
 import Navbar from "@/components/Navbar";
 import TimeBanner from "@/app/components/dashboard/TimeBanner";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { TypeSingleTournament } from "../../../../../types/global";
+import { useParams } from "next/navigation";
+import { getFn } from "@/lib/apiClient";
+import FullScreenLoader from "@/app/components/dashboard/FullScreenLoader";
 
 const CreateWager = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [data, setData] = useState<TypeSingleTournament>();
+  const params = useParams();
+  const slug = params?.slug;
+
+  const getTournament = async () => {
+    setLoading(true);
+    try {
+      const response: TypeSingleTournament = await getFn(
+        `/api/tournamentstables/view/${slug}`
+      );
+      if (response?.id) {
+        setData(response);
+      }
+    } catch {
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  console.log(data);
+  useEffect(() => {
+    getTournament();
+  }, [slug]);
   return (
     <>
       <Navbar variant="primary" />
-      <TimeBanner />
-      <CreateWagerBanner />
+      {loading ? (
+        <FullScreenLoader isLoading={true} text="Loading Tournament Details" />
+      ) : (
+        <>
+          <TimeBanner />
+          <CreateWagerBanner />
+        </>
+      )}
     </>
   );
 };
--- types/global.ts
+++ types/global.ts
@@ -16,3 +16,25 @@ export type TypeCategories = {
   updated_at: string | null;
   tournaments: [];
 };
+export type TypeGames = {
+  category_id: string;
+  description: string;
+  game_image: string;
+  id: string;
+  name: string;
+  created_at: string | null;
+  updated_at: string | null;
+};
+export type TypeSingleTournament = {
+  amount: string;
+  bet_on: string;
+  game_image: string;
+  category_id: string;
+  description: string;
+  id: string;
+  match_time: string;
+  number_of_participants: string;
+  user_id: number;
+  created_at: string | null;
+  updated_at: string | null;
+};
